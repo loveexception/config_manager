4d38e159a849d43a4e6264b37b2abd4f6d940ae8
maodajun <maodj@tico.cn>
2019-07-07 15:37:47 +0800

    grader

--- src/main/java/cn/tico/iot/configmanger/iot/services/GradeService.java
+++ src/main/java/cn/tico/iot/configmanger/iot/services/GradeService.java
@@ -2,7 +2,6 @@ package cn.tico.iot.configmanger.iot.services;
 
 
 import cn.tico.iot.configmanger.common.base.Service;
-import cn.tico.iot.configmanger.iot.models.driver.Driver;
 import cn.tico.iot.configmanger.iot.models.driver.Grade;
 import org.nutz.dao.Dao;
 import org.nutz.ioc.loader.annotation.IocBean;
