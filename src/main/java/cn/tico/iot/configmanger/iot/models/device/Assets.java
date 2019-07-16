7a0d3164f5270636a5913f7fd79e287e8eee5a6a
maodajun <maodj@tico.cn>
2019-07-16 11:34:40 +0800

    tag 业务标签。

--- src/main/java/cn/tico/iot/configmanger/iot/models/device/Assets.java
+++ src/main/java/cn/tico/iot/configmanger/iot/models/device/Assets.java
@@ -12,24 +12,12 @@ import java.io.Serializable;
 
 @Data
 @Table("t_iot_assets")
-public class Assets extends I18NModel implements Serializable {
+public class Assets extends Device implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
 
 
-    /**
-     * 组织
-     */
-    @Column("device_id")
-    @Comment("组织")
-    private String deviceid;
-
-    /**
-     * 组织
-      */
-    @One(field = "deviceid",key = "id")
-    private Device device;
 
 
 
