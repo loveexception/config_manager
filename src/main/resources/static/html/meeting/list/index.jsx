b94e550ad326aca91bdea607f22f05ec2b024ebe
505521261@qq.com <cuiyh@tico.cn>
2020-04-14 18:12:50 +0800

    fix:' meeting page  '

--- src/main/resources/static/html/meeting/list/index.jsx
+++ src/main/resources/static/html/meeting/list/index.jsx
@@ -1,8 +1,96 @@
-let { PureComponent, useState } = React;
+let { useRef, useState, useEffect, useLayoutEffect, PureComponent } = React;
+let allHeight = 150;// 图高度设置 不包括饼图
 let DiagramAction = window.DiagramAction;
-let { Popconfirm, Upload, Form, message, Icon, Input, InputNumber, Button, Table, Tooltip, Modal } = antd;
+let { Popconfirm, Upload, Form, message, Icon, Input, InputNumber, Button, Table, Tooltip, Modal, DatePicker, BackTop } = antd;
 let { confirm } = Modal;
 let Message = message;
+let { RangePicker } = DatePicker;
+// 日历
+class DateRange extends React.Component {
+	state = {
+		startValue: null,
+		endValue: null,
+		endOpen: false,
+	};
+
+	disabledStartDate = startValue => {
+		const { endValue } = this.state;
+		if (!startValue || !endValue) {
+			return false;
+		}
+		return startValue.valueOf() > endValue.valueOf();
+	};
+
+	disabledEndDate = endValue => {
+		const { startValue } = this.state;
+		if (!endValue || !startValue) {
+			return false;
+		}
+		return endValue.valueOf() <= startValue.valueOf();
+	};
+
+	onChange = (field, value) => {
+		this.setState({
+			[field]: value,
+		});
+	};
+
+	onStartChange = value => {
+		this.onChange('startValue', value);
+	};
+
+	onEndChange = value => {
+		this.onChange('endValue', value);
+	};
+
+	handleStartOpenChange = open => {
+		if (!open) {
+			this.setState({ endOpen: true });
+		}
+	};
+
+	handleEndOpenChange = open => {
+		this.setState({ endOpen: open });
+	};
+
+	render() {
+		const { startValue, endValue, endOpen } = this.state;
+		return (
+			<div style={{
+				display: 'flex',
+				textAlign: 'center',
+				lineHeight: '0.3rem'
+			}}>
+				<span className="data-title">会议数据统计表</span>
+				&nbsp;&nbsp;&nbsp;&nbsp;
+				<DatePicker
+					disabledDate={this.disabledStartDate}
+					showTime
+					format="YYYY-MM-DD HH:mm"
+					value={startValue}
+					placeholder="开始时间"
+					onChange={this.onStartChange}
+					onOpenChange={this.handleStartOpenChange}
+				/>
+					至
+				<DatePicker
+					ranges={{
+						Today: [moment(), moment()],
+						'This Month': [moment().startOf('month'), moment().endOf('month')],
+					}}
+					disabledDate={this.disabledEndDate}
+					showTime
+					format="YYYY-MM-DD HH:mm"
+					value={endValue}
+					placeholder="结束时间"
+					onChange={this.onEndChange}
+					open={endOpen}
+					onOpenChange={this.handleEndOpenChange}
+				/>
+			</div>
+		);
+	}
+}
 // modal 显示
 function showConfirm(_ok, _EditableTable) {
 	confirm({
@@ -60,7 +148,7 @@ const ButtonGroup = Button.Group;
 let Search = Input.Search;
 
 let UpDataData = null;
-const pageSize = 6;
+const pageSize = 4; //条数
 const align = 'left';
 
 // 删除调用函数
@@ -487,14 +575,13 @@ class EditableTable extends React.Component {
 					dataSource={this.props ? this.props.data : []}
 					columns={columns}
 					rowClassName="editable-row"
-
 					pagination={{
-
 						pageSize,
 						current: _pagination.current,
 						total: _pagination.total,
 						defaultCurrent: 1,
 						onChange: this.paginationChange,
+						position: 'bottom'
 						// pageSizeOptions: ,
 					}}
 					rowKey={record => record.id}
@@ -792,5 +879,290 @@ function Topo() {
 	);
 }
 
-ReactDOM.render(<Topo />, window.rootNode);
-// React.render() Topo;
+
+
+
+
+//圆柱图 && 饼图
+function VisualTop(props) {
+	let [count, setCount] = useState(1);
+	// 圆柱
+	useEffect(() => {
+		G2.registerShape('interval', 'border-radius', {
+			draw(cfg, container) {
+				const points = cfg.points;
+				let path = [];
+				path.push(['M', points[0].x, points[0].y]);
+				path.push(['L', points[1].x, points[1].y]);
+				path.push(['L', points[2].x, points[2].y]);
+				path.push(['L', points[3].x, points[3].y]);
+				path.push('Z');
+				path = this.parsePath(path); // 将 0 - 1 转化为画布坐标
+
+				const group = container.addGroup();
+				group.addShape('rect', {
+					attrs: {
+						x: path[1][1], // 矩形起始点为左上角
+						y: path[1][2],
+						width: path[2][1] - path[1][1],
+						height: path[0][2] - path[1][2],
+						fill: cfg.color,
+						radius: (path[2][1] - path[1][1]) / 2,
+					},
+				});
+
+				return group;
+			},
+		});
+		const data = [
+			{ year: '1991', value: 3 },
+			{ year: '1992', value: 4 },
+			{ year: '1993', value: 3.5 },
+			{ year: '1994', value: 5 },
+		];
+		const chart = new G2.Chart({
+			container: 'circle',
+			// width: 50,
+			//	自适应
+			autoFit: true,
+			height: allHeight,
+			// width: 15
+		});
+
+		chart.data(data);
+		chart.tooltip({
+			//showMarkers: false
+		});
+		// 滑块选取
+		// chart.interaction('brush');
+		// 控制大小site
+		chart.interval().position('year*value').color('l(90) 0:#4c71fe 1:rgba(76, 113, 254, 0.64) ').size(5).shape('date*actual', (date, val) => {
+			if (val === 0) {
+				return;
+			}
+			return 'border-radius';
+		});
+		chart.render();
+	}, [])
+	//饼图
+
+	useEffect(() => {
+		const data = [
+			{ item: '事例一', count: 40, percent: 0.4 },
+			{ item: '事例二', count: 21, percent: 0.21 },
+			{ item: '事例三', count: 17, percent: 0.17 },
+			{ item: '事例四', count: 13, percent: 0.13 },
+			{ item: '事例五', count: 9, percent: 0.09 },
+		];
+
+		const chart = new G2.Chart({
+			container: 'pie',
+			// width: 250,
+			autoFit: true,
+			height: 220,
+		});
+
+		chart.coordinate('theta', {
+			radius: 0.75,
+		});
+
+		chart.data(data);
+
+		chart.scale('percent', {
+			formatter: (val) => {
+				val = val * 100 + '%';
+				return val;
+			},
+		});
+		chart.legend({
+			position: 'right',
+			offsetX: -20
+		});
+		chart.tooltip({
+			showTitle: false,
+			showMarkers: false,
+		});
+
+		chart
+			.interval()
+			.position('percent')
+			.color('item')
+			.label('percent', {
+				offset: -20,
+				style: {
+					textAlign: 'center',
+					fontSize: 12,
+					shadowBlur: 2,
+					shadowColor: 'rgba(0, 0, 0, .45)',
+					fill: '#fff',
+				},
+			})
+			.adjust('stack');
+
+		chart.interaction('element-active');
+
+		chart.render();
+	}, [])
+	return <div className="visual-top-box">
+		<div className="circle-box padding-view">
+			<div className="view-title">
+				会议级别时长
+		</div>
+			<span className="view-unit">
+				单位: 小时
+		</span>
+			<div id="circle" >
+
+			</div>
+		</div>
+		<div className="pie-box padding-view">
+			<div className="view-title">
+				会议级别统计
+		</div>
+			<span className="view-unit">
+				单位: 次
+		</span>
+			<div className="all-count">
+				<div className="all-title">总次数
+				<br />
+					<span className="all-number">{count}</span></div>
+			</div>
+			<div id="pie"></div>
+		</div>
+	</div >
+
+
+
+
+}
+
+// 折线图
+function VisualBottom(props) {
+	useEffect(() => {
+		const data = [
+			{ year: '1991', value: 3 },
+			{ year: '1992', value: 4 },
+			{ year: '1993', value: 3.5 },
+			{ year: '1994', value: 5 },
+			{ year: '1995', value: 4.9 },
+			{ year: '1996', value: 6 },
+			{ year: '1997', value: 7 },
+			{ year: '1998', value: 9 },
+			{ year: '1999', value: 13 },
+		];
+		const chart = new G2.Chart({
+			container: 'line-content',
+			autoFit: true,
+
+			height: allHeight,
+		});
+
+		chart.data(data);
+		chart.scale({
+			year: {
+				range: [0, 1],
+			},
+			value: {
+				min: 0,
+				nice: true,
+			},
+		});
+
+		chart.tooltip({
+			showCrosshairs: true, // 展示 Tooltip 辅助线
+			shared: true,
+		});
+
+		chart.line().position('year*value').label('value');
+		chart.point().position('year*value');
+		chart.area().position('year*value').style({
+			// fill: 'r(0.5,1.3,1.3) 0:#ffffff 1:#1890ff',
+			fill: "l(90) 0:#1890FF 1:#f7f7f7",
+			//stroke: 'l(0) 0:#ffffff 0.5:#7ec2f3 1:#1890ff'
+		})
+		chart.tooltip({
+			// showCrosshairs: true, // 展示 Tooltip 辅助线
+			shared: true,
+			showTitle: false,
+			itemTpl: '<li class="test">{year} 有 {value} 个</li>',
+		});
+
+		chart.render();
+	}, [])
+	return <div className="line-box ">
+		<div className="line-title view-title">
+			会议保障时长
+		</div>
+		<span className="line-unit view-unit">
+			单位: 小时
+		</span>
+		<div id="line-content">
+		</div>
+	</div>
+}
+function App() {
+	let node = useRef(null);
+	let [isShowBtn, setIsShowBtn] = useState(false)
+	function handleScroll(e) {
+
+		node.current.scrollTop > 0 ? setIsShowBtn(true) : setIsShowBtn(false)
+	}
+	useEffect(function () {
+		// chart.on('tooltip:show', (ev) => {
+		// 	// x: 当前鼠标的 x 坐标,
+		// 	// y: 当前鼠标的 y 坐标,
+		// 	// tooltip: 当前的 tooltip 对象
+		// 	// items: 数组对象，当前 tooltip 显示的每条内容
+		// 	// title: tooltip 标题
+		// 	const { tooltip, items, title, x, y } = ev;
+		// });
+	}, [])
+
+
+	return <div onScroll={handleScroll} className="meeting-list-box" style={{
+		// height: '200%'
+	}} ref={node}>
+		<div className="to-top" style={{
+			position: 'fixed',
+			right: '0',
+			bottom: '0.7rem'
+		}}>
+			<Button style={{
+				display: isShowBtn ? '' : 'none'
+			}} onClick={function () {
+				node.current.scroll(0, 0)
+			}}>
+				<Icon type="to-top" />
+			</Button>
+		</div>
+		<div className="table-box">
+			<Topo />
+		</div>
+		<div className="visual-box">
+			<div className="visual-select-box">
+				<DateRange />
+			</div>
+			<div className="visual-top">
+				<VisualTop />
+			</div>
+			<div className="visual-bottom padding-view">
+				<VisualBottom />
+			</div>
+
+		</div>
+
+	</div>
+}
+
+ReactDOM.render(<App />, window.rootNode);
+
+
+
+// ReactDOM.render(<div style={{
+// 	height: '300%'
+// }}>
+
+// 	Scroll down to see the bottom-right
+// <strong style={{ color: '#1088e9' }}> blue </strong>
+// button.
+// </div>, window.rootNode)
---
+++ src/main/resources/static/html/meeting/list/reqlist.js
@@ -0,0 +1,155 @@
+(function (w) {
+	// 地址
+	const href = 'http://172.16.16.9/api/backgroundinterface/';
+	w.DiagramAction = {
+		diagramList,
+		diagramuploadImg,
+		diagramRename,
+		diagramViewImg,
+		diagramODownImg,
+		diagramORmImg,
+		diagramMRmImg,
+		diagramMDownImg,
+		diagramListSort
+	};
+	function getDeptId() {
+		let id = localStorage.getItem('deptId');
+		if (id === null) {
+			console.log('deptId 为空 请重新登陆')
+		}
+		return id
+	}
+	function diagramListSort(params, callback) {
+		$.ajax({
+			cache: true,
+			type: 'POST',
+			url: href + 'topology/list',
+			data: JSON.stringify(params),
+			dataType: 'json',
+			async: false,
+			// 加东西 
+			headers: { 'Content-Type': 'application/json;charset=utf8', 'dept_id': getDeptId() },
+			contentType: "application/json",
+			success: callback
+		})
+
+	}
+	function diagramList(params, callback) {
+		$.ajax({
+			cache: true,
+			type: 'POST',
+			url: href + 'topology/list',
+			data: JSON.stringify(params),
+			dataType: 'json',
+			async: false,
+			// 加东西 
+
+			headers: { 'Content-Type': 'application/json;charset=utf8', 'dept_id': getDeptId() },
+			contentType: "application/json",
+			success: callback
+		})
+	}
+	// 上传图片
+	function diagramuploadImg(params, callback) {
+		$.ajax({
+			cache: true,
+			type: 'POST',
+			contentType: "application/json",
+			url: href + 'topology/upload',
+			data: JSON.stringify(params),
+			dataType: 'json',
+			headers: { 'Content-Type': 'application/json;charset=utf8', 'dept_id': getDeptId() },
+
+			async: false,
+			success: callback
+		})
+	}
+	//重命名
+	function diagramRename(params, callback) {
+		$.ajax({
+			cache: true,
+			type: 'POST',
+			url: href + 'topology/renameById',
+			contentType: "application/json",
+			data: JSON.stringify(params),
+			headers: { 'Content-Type': 'application/json;charset=utf8', 'dept_id': getDeptId() },
+			dataType: 'json',
+			async: false,
+			success: callback
+		})
+	}
+	// 点击显示图片
+	function diagramViewImg(params, callback) {
+		$.ajax({
+			cache: true,
+			type: 'POST',
+			url: href + 'topology/preview',
+			contentType: "application/json",
+			data: JSON.stringify(params),
+			headers: { 'Content-Type': 'application/json;charset=utf8', 'dept_id': getDeptId() },
+			dataType: 'json',
+			async: false,
+			success: callback
+		})
+	}
+	// 点击下载图片 多
+	function test() {
+	}
+	function diagramMDownImg(params, callback) {
+		$.ajax({
+			cache: true,
+			type: 'POST',
+			url: href + 'topology/beachDownImg',
+			data: JSON.stringify(params),
+			dataType: 'json',
+			contentType: "application/json",
+			headers: { 'Content-Type': 'application/json;charset=utf8', 'dept_id': getDeptId() },
+			async: false,
+			success: callback
+		})
+	}
+	// 点击下载图片 单
+	function diagramODownImg(params, callback) {
+		$.ajax({
+			cache: true,
+			type: 'GET',
+			url: href + 'topology/downImg',
+			data: JSON.stringify(params),
+			contentType: "application/json",
+			headers: { 'Content-Type': 'application/json;charset=utf8', 'dept_id': getDeptId() },
+			dataType: 'json',
+			async: false,
+			success: callback
+		})
+	}
+	// 点击删除图片 单
+	function diagramORmImg(params, callback) {
+		$.ajax({
+			cache: true,
+			type: 'POST',
+			contentType: "application/json",
+			url: href + 'topology/deleteByPrimaryKey',
+			data: JSON.stringify(params),
+			headers: { 'Content-Type': 'application/json;charset=utf8', 'dept_id': getDeptId() },
+			dataType: 'json',
+			async: false,
+			success: callback
+		})
+	}
+	// 点击删除图片 多
+	function diagramMRmImg(params, callback) {
+		$.ajax({
+			cache: true,
+			type: 'POST',
+			url: href + 'topology/beachDel',
+			data: JSON.stringify(params),
+			contentType: "application/json",
+			headers: { 'Content-Type': 'application/json;charset=utf8', 'dept_id': getDeptId() },
+			dataType: 'json',
+			async: false,
+			success: callback
+		})
+	}
+	//
+
+})(window)
